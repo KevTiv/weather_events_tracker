48230a1edcc689b249aa937a9a1a19afd56bd629
Kevin Tivert <tivertc.kevin@outlook.com>
2022-02-11 15:21:11 +0200

    -mgoogle map api required payement method, switch to a free map api

--- src/Components/Map.tsx
+++ src/Components/Map.tsx
@@ -2,6 +2,7 @@ import {useState} from 'react';
 import GoogleMapReact from 'google-map-react';
 import {LocationFireMarker, LocationVolcanoMarker, LocationStormMarker} from './LocationMarker';
 import LocationInfoBox from './LocationInfoBox';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 //Props defintions for TS.
 type mapProps = {
@@ -28,18 +29,24 @@ const Map = ({eventData, center, zoom, showFires, showVolcanos, showStorms}: map
     const fireMarkers = eventData.map((event:any) =>{
         if(event.categories[0].id === 'wildfires'){
             return(
-                <LocationFireMarker
-                    key={event.id}  
-                    lat={event.geometry[0].coordinates[1]} 
-                    lng={event.geometry[0].coordinates[0]} 
-                    onClick={()=>{
-                        setEventLocationInfo({
-                            id: event.id,
-                            details: event.title
-                        })
+                // <LocationFireMarker
+                //     key={event.id}  
+                //     lat={event.geometry[0].coordinates[1]} 
+                //     lng={event.geometry[0].coordinates[0]} 
+                //     onClick={()=>{
+                //         setEventLocationInfo({
+                //             id: event.id,
+                //             details: event.title
+                //         })
                         
-                    }}
-                />
+                //     }}
+                // />
+                <Marker position={[event.geometry[0].coordinates[1],event.geometry[0].coordinates[0]]}>
+                    <Popup>
+                        <h2>id: {event.id},</h2><br/>
+                        <h2>details: {event.title}</h2>
+                    </Popup>
+                </Marker>
             )
         }else{
             return null;
@@ -50,18 +57,24 @@ const Map = ({eventData, center, zoom, showFires, showVolcanos, showStorms}: map
     const volcanoMarkers = eventData.map((event:any) =>{
         if(event.categories[0].id === 'volcanoes'){
             return(
-                <LocationVolcanoMarker
-                    key={event.id}  
-                    lat={event.geometry[0].coordinates[1]} 
-                    lng={event.geometry[0].coordinates[0]} 
-                    onClick={()=>{
-                        setEventLocationInfo({
-                            id: event.id,
-                            details: event.title
-                        })
+                // <LocationVolcanoMarker
+                //     key={event.id}  
+                //     lat={event.geometry[0].coordinates[1]} 
+                //     lng={event.geometry[0].coordinates[0]} 
+                //     onClick={()=>{
+                //         setEventLocationInfo({
+                //             id: event.id,
+                //             details: event.title
+                //         })
                         
-                    }}
-                />
+                //     }}
+                // />
+                <Marker position={[event.geometry[0].coordinates[1],event.geometry[0].coordinates[0]]}>
+                    <Popup>
+                        <h2>id: {event.id},</h2><br/>
+                        <h2>details: {event.title}</h2>
+                    </Popup>
+                </Marker>
             )
         }else{
             return null;
@@ -71,18 +84,25 @@ const Map = ({eventData, center, zoom, showFires, showVolcanos, showStorms}: map
     const stormMarkers = eventData.map((event:any) =>{
         if(event.categories[0].id === 'severeStorms'){
             return(
-                <LocationStormMarker
-                    key={event.id}  
-                    lat={event.geometry[0].coordinates[1]} 
-                    lng={event.geometry[0].coordinates[0]} 
-                    onClick={()=>{
-                        setEventLocationInfo({
-                            id: event.id,
-                            details: event.title
-                        })
+                // <LocationStormMarker
+                //     key={event.id}  
+                //     lat={event.geometry[0].coordinates[1]} 
+                //     lng={event.geometry[0].coordinates[0]} 
+                //     onClick={()=>{
+                //         setEventLocationInfo({
+                //             id: event.id,
+                //             details: event.title
+                //         })
                         
-                    }}
-                />
+                //     }}
+                // />
+                <Marker position={[event.geometry[0].coordinates[1],event.geometry[0].coordinates[0]]}>
+                    <Popup>
+                        <h2>id: {event.id},</h2><br/>
+                        <h2>details: {event.title}</h2>
+                    </Popup>
+                </Marker>
+
             )
         }else{
             return null;
@@ -91,7 +111,7 @@ const Map = ({eventData, center, zoom, showFires, showVolcanos, showStorms}: map
     return (
         <>
             <div className="map-container">
-                <div className="google-map">
+                {/* <div className="google-map">
                     <GoogleMapReact
                         bootstrapURLKeys = {{key:googleMapApiKey!}}
                         defaultCenter={center}
@@ -101,8 +121,17 @@ const Map = ({eventData, center, zoom, showFires, showVolcanos, showStorms}: map
                         {(showVolcanos) ? volcanoMarkers : <></>}
                         {(showStorms) ? stormMarkers : <></>}
                     </GoogleMapReact>
-                </div>
-                {eventLocationInfo && <LocationInfoBox info={eventLocationInfo}/>}
+                </div> */}
+                <MapContainer center={[center.lat, center.lng]} zoom={zoom}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                        {(showFires) ? fireMarkers : <></>}
+                        {(showVolcanos) ? volcanoMarkers : <></>}
+                        {(showStorms) ? stormMarkers : <></>}
+                </MapContainer>
+                {/* {eventLocationInfo && <LocationInfoBox info={eventLocationInfo}/>} */}
             </div>
         </>
         
@@ -115,6 +144,6 @@ Map.defaultProps = {
         lat: 41.926701,
         lng: 8.736900
     },
-    zoom:6
+    zoom:4
 }
 export default Map
